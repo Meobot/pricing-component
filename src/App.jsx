c57c3047e5077f81e4f58d9a8d8c8fb6d1fb6df6
Meobot <nathanrhodes312@gmail.com>
2023-02-03 16:58:08 -0500

    Header deleted / Slider created / renders data

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,37 @@
 import { useState } from 'react'
-import Header from './components/Header'
+import React from 'react'
+import Card from './components/Card'
+import Slider from './components/Slider'
+import Data from "./data"
 
 function App() {
-	const [count, setCount] = useState(0)
+	const [sliderValue, setSliderValue] = useState(false);
+
+	const updateSliderValue = (value) => {
+   		setSliderValue(value);
+  	}
+
+	const cards = Data.map(item => {
+		return (
+			<Card 
+				key={item.id}
+				sliderValue={sliderValue}
+				{...item}
+			/>
+		)
+	})
 
 	return (
 		<div className="App">
-			<Header />
+			<div className="Header-container">
+				<h1 className="Header-title">Our Pricing</h1>
+				<div className="toggle-container">
+					<h2 className="toggle-h2">Annually</h2>
+						<Slider updateSliderValue={updateSliderValue}/>
+					<h2 className="toggle-h2">Monthly</h2>
+				</div>
+			</div>
+			{cards}
 		</div>
 	)
 }
