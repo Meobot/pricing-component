b82f142b3ab0bea52c268860671a84f803db799c
Meobot <nathanrhodes312@gmail.com>
2023-02-03 19:58:11 -0500

    mobile version done

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -9,13 +9,18 @@ const Card = (props) => {
 	}
 
 	return (
-		<div className="Card">
+		<div className={"Card " + `Card-${props.id}`}>
 			<h2 className="Card-title">{props.title}</h2>
 			<p className="Card-price">{price}</p>
 			<div className="Card-details-container">
+				<hr/>
 				<p>{props.storage} Storage</p>
+				<hr/>
 				<p>{props.usersAllowed} Users Allowed</p>
+				<hr/>
 				<p>Send up to {props.dataLimit}</p>
+				<hr/>
+				<h3 className={"learn-more-h3 " + `Card-${props.id}-learn-more-h3`}>LEARN MORE</h3>
 			</div>
 		</div>
 	)
